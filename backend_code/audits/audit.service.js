53e0fd9aede80b47bdfab81a669bd0e76a46bb9c
Karthik <ksb.karthik241@gmail.com>
2021-09-19 17:55:28 +0530

    backend code

---
+++ backend_code/audits/audit.service.js
@@ -0,0 +1,41 @@
+﻿const config = require('config.js');
+const jwt = require('jsonwebtoken');
+const Role = require('_helpers/role');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+const Audit = db.Audit;
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
+};
+
+async function getAll() {
+    return await Audit.find().select();
+}
+
+async function getById(id) {
+    return await Audit.findById(id).select();
+}
+
+async function create(auditParam) {
+    const audit = new Audit(auditParam);
+
+    return await audit.save();
+}
+
+async function update(id, auditParam) {
+    const audit = await Audit.findById(id);
+
+    // copy userParam properties to Audit
+    Object.assign(audit, auditParam);
+
+    await audit.save();
+}
+
+async function _delete(id) {
+    await Audit.findByIdAndRemove(id);
+}
---
+++ backend_code/audits/audits.controller.js
@@ -0,0 +1,51 @@
+﻿const express = require('express');
+const router = express.Router();
+const auditService = require('./audit.service');
+const authorize = require('_helpers/authorize')
+const Role = require('_helpers/role');
+
+// routes
+router.get('/', authorize(Role.Auditor), getAll);
+router.post('/', authorize(Role.Auditor), create);
+router.get('/:id', authorize(Role.Auditor), getById);
+router.put('/:id', authorize(Role.Auditor), update);
+router.delete('/:id', authorize(Role.Auditor), _delete);
+
+module.exports = router;
+
+function create(req, res, next) {
+    auditService.create(req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function getAll(req, res, next) {
+    auditService.getAll()
+        .then(audits => res.json(audits))
+        .catch(err => next(err));
+}
+
+function getById(req, res, next) {
+    // only allow admins to access other user records
+    if (id !== currentUser.sub && currentUser.role !== Role.Auditor) {
+        return res.status(401).json({
+            message: 'Unauthorized'
+        });
+    }
+
+    auditService.getById(req.params.id)
+        .then(audit => audit ? res.json(audit) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function update(req, res, next) {
+    auditService.update(req.params.id, req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function _delete(req, res, next) {
+    auditService.delete(req.params.id)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
